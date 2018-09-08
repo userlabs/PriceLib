b2756c85bd57a2a5f046d782abe797f2dbdd0bf1
nayar <nayarlabs@gmail.com>
2018-09-08 18:41:40 -0400

    Added validations for inputs

--- get-price.js
+++ get-price.js
@@ -3,7 +3,7 @@
  * 
  */
 
-const provinceTax = {
+const provinceData = {
 	AB: { name: "Alberta", rate: 5 },
 	ON: { name: "Ontario", rate: 13 },
 	QC: { name: "Quebec", rate: 14.975 },
@@ -18,6 +18,43 @@ const orderValue = {
 	"10000": 10
 }
 
-module.exports = function getPrice( quantity, price, province) {
-	console.log('input:', quantity+' items', ',$'+price+'per item', province);
+const inputValidations = {
+	quantity: function ( quantity ) {
+		console.log('quantity', quantity);
+		if(!isNaN(quantity) || !isNaN(parseInt(quantity))) {
+			return true;
+		}
+		return false;
+	},
+	
+	price: function ( price ) {
+		var p = parseInt(price);
+		if(!isNaN(p) || !isNaN(parseInt(price))) {
+			return true;
+		}
+		return false;
+	},
+	
+	provinceCode: function (provinceCode) {
+		if(provinceData[provinceCode] === undefined) {
+			return false;
+		}
+		return true;
+	}
+	
+}
+
+module.exports = function getPrice( quantity, price, provinceCode) {
+	
+	if(!inputValidations.quantity(quantity)) {
+		throw new Error("Invalid quantity: usage[quantity should be a number]");
+	}
+	if(!inputValidations.price(price)) {
+		throw new Error("Invalid price: usage[price should be a number]");
+	}
+	if(!inputValidations.provinceCode(provinceCode)) {
+		throw new Error("Invalid provinceCode: usage[provinceCode should be "+Object.keys(provinceData).join()+" ]");
+	}
+	console.log('input:', quantity+' items', ',$'+price+'per item', provinceData[provinceCode].name);
+	
 }
--- test/get-price-test.js
+++ test/get-price-test.js
@@ -1,11 +1,39 @@
 'use strict;'
 
-var getPrice = require('../get-price');
+const getPrice = require('../get-price');
+const assert = require('assert');
 
 describe( 'Test suite', function( done) {
 	
-	it('getPrice', function(done) {
+	it('quantity and price values as numbers', function(done) {
 		getPrice(10, 20, 'ON');
 		done();
 	});
+	
+	it('quantity and price values as strings', function(done) {
+		getPrice('10', '20', 'ON');
+		done();
+	});
+
+	it('quantity value is invalid', function(done) {
+		assert.throws(function() {
+			getPrice('i', 20, 'ON');
+		}, Error('Invalid quantity: usage[quantity should be a number]'));
+		done();
+	});
+	
+	it('price value is invalid', function(done) {
+		assert.throws(function() {
+			getPrice(10, 'i', 'ON');
+		}, Error('Invalid price: usage[price should be a number]'));
+		done();
+	});
+
+	it('province code is invalid', function(done) {
+		assert.throws(function() {
+			getPrice(10, 20, 'FL');
+		}, Error);
+		done();
+	});
+	
 });
