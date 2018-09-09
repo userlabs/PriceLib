abcc50b3920901bccf4b4755bdc0dcba2e5b0015
nayar <nayarlabs@gmail.com>
2018-09-08 20:08:40 -0400

    Added logic to calculate totalCost and unit test cases.

--- get-price.js
+++ get-price.js
@@ -4,33 +4,67 @@
  */
 
 const provinceData = {
-	AB: { name: "Alberta", rate: 5 },
-	ON: { name: "Ontario", rate: 13 },
-	QC: { name: "Quebec", rate: 14.975 },
-	MI: { name: "Michigan", rate: 6 },
-	DE: { name: "Detroit", rate: 0 }
+	AB: { name: "Alberta", taxRate: 5.0 },
+	ON: { name: "Ontario", taxRate: 13.0 },
+	QC: { name: "Quebec", taxRate: 14.975 },
+	MI: { name: "Michigan", taxRate: 6.0 }, //todo: USD exchange rate?
+	DE: { name: "Detroit", taxRate: 0.0 } // todo: USD exchange rate?
 }
 
-const orderValue = {
-	"1000": 3,
-	"5000": 5,
-	"7000": 7,
-	"10000": 10
+const discounts = {
+	1000: 3.0,
+	5000: 5.0,
+	7000: 7.0,
+	10000: 10.0
 }
+
+const getDiscountRate = function () {
+
+	// cache the qty keys in reverse order 10000, 7000 etc.
+	var qtys = Object.keys(discounts);
+	// convert them to numbers - not doing this caused the matching below to give weird behavior
+	for(i=0; i < qtys.length; i++) {
+		qtys[i] = parseInt(qtys[i])
+	}
+	// number matching when sorting
+	qtys.sort(function(a,b) {
+		return a - b;
+	}).reverse();
+
+	// use a closure to return discount rates
+	return function(quantity) {
+		for(i=0; i < qtys.length; i++) {
+			console.log('quantity', quantity, 'qtys', qtys[i]);
+			if(quantity >= qtys[i]){
+				return discounts[qtys[i]];
+			}
+		}
+		return 0;
+	}
+}();
+
+
 
 const inputValidations = {
 	quantity: function ( quantity ) {
-		console.log('quantity', quantity);
-		if(!isNaN(quantity) || !isNaN(parseInt(quantity))) {
-			return true;
+		if(!isNaN(quantity)) {
+			return quantity;
+			quantity = parseInt(quantity)
+			if(!isNaN(quantity)) {
+				return quantity;
+			}
 		}
 		return false;
 	},
 	
 	price: function ( price ) {
 		var p = parseInt(price);
-		if(!isNaN(p) || !isNaN(parseInt(price))) {
-			return true;
+		if(!isNaN(price)) {
+			return price;
+			price = parseInt(price);
+			if(!isNaN(price)) {
+				return price;
+			}
 		}
 		return false;
 	},
@@ -45,16 +79,28 @@ const inputValidations = {
 }
 
 module.exports = function getPrice( quantity, price, provinceCode) {
-	
-	if(!inputValidations.quantity(quantity)) {
+
+	var quantityVal = inputValidations.quantity(quantity);
+	if(false === quantityVal) {
 		throw new Error("Invalid quantity: usage[quantity should be a number]");
 	}
-	if(!inputValidations.price(price)) {
+	var priceVal = inputValidations.price(price);
+	if(false === priceVal) {
 		throw new Error("Invalid price: usage[price should be a number]");
 	}
 	if(!inputValidations.provinceCode(provinceCode)) {
 		throw new Error("Invalid provinceCode: usage[provinceCode should be "+Object.keys(provinceData).join()+" ]");
 	}
-	console.log('input:', quantity+' items', ',$'+price+'per item', provinceData[provinceCode].name);
+	console.log('input:', quantityVal+' items', ',$'+priceVal+'per item', provinceData[provinceCode].name);
+
+	var discountRate = getDiscountRate(quantityVal.toString());
+	var taxRate = provinceData[provinceCode].taxRate;
+	console.log('tax', taxRate, 'discount', discountRate);
+	var totalCost = quantityVal * priceVal;
+	var totalCostDiscounted = totalCost - ((totalCost*discountRate)/100);
+	var totalCostTaxed = totalCostDiscounted + ((totalCostDiscounted*taxRate)/100);
+	totalCostTaxed = totalCostTaxed.toFixed(2); // resolved to 2 decimals
+	
+	return totalCostTaxed;
 	
 }
--- test/get-price-test.js
+++ test/get-price-test.js
@@ -35,5 +35,70 @@ describe( 'Test suite', function( done) {
 		}, Error);
 		done();
 	});
+
+	it('totalCost q=10,p=0,pr=ON', function(done) {
+		assert.equal(getPrice(10, 0, 'ON'),0);
+		done();
+	});
+	it('totalCost q=0,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(0, 20, 'ON'),0);
+		done();
+	});
+	it('totalCost q=1,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(1, 20, 'ON'),(1*20*(1.13)).toFixed(2));
+		done();
+	});
+	it('totalCost q=999,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(999, 20, 'ON'),(999*20*(1.13)).toFixed(2));
+		done();
+	});
+
+	it('totalCost q=1000,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(1000, 20, 'ON'),(1000*20*(0.97)*(1.13)).toFixed(2));
+		done();
+	});
+	it('totalCost q=1001,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(1001, 20, 'ON'),(1001*20*(0.97)*(1.13)).toFixed(2));
+		done();
+	});
+	it('totalCost q=4999,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(4999, 20, 'ON'),(4999*20*(0.97)*(1.13)).toFixed(2));
+		done();
+	});
+
+	it('totalCost q=5000,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(5000, 20, 'ON'),(5000*20*(0.95)*(1.13)).toFixed(2));
+		done();
+	});
+	it('totalCost q=5001,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(5001, 20, 'ON'),(5001*20*(0.95)*(1.13)).toFixed(2));
+		done();
+	});
+	it('totalCost q=6999,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(6999, 20, 'ON'),(6999*20*(0.95)*(1.13)).toFixed(2));
+		done();
+	});
+
+	it('totalCost q=7000,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(7000, 20, 'ON'),(7000*20*(0.93)*(1.13)).toFixed(2));
+		done();
+	});
+	it('totalCost q=7001,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(7001, 20, 'ON'),(7001*20*(0.93)*(1.13)).toFixed(2));
+		done();
+	});
+	it('totalCost q=9999,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(9999, 20, 'ON'),(9999*20*(0.93)*(1.13)).toFixed(2));
+		done();
+	});
+
+	it('totalCost q=10000,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(10000, 20, 'ON'),(10000*20*(0.9)*(1.13)).toFixed(2));
+		done();
+	});
+	it('totalCost q=10001,p=20,pr=ON', function(done) {
+		assert.equal(getPrice(10001, 20, 'ON'),(10001*20*(0.9)*(1.13)).toFixed(2));
+		done();
+	});
 	
 });
